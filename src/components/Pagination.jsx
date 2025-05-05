7d94d9f6a05bd49efdc59a1aea99b75d81785533
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-05-05 08:39:21 +0100

    added spacing between pagination links

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,13 +2,19 @@ const Pagination = () => {
 	return (
 		<div className="col-lg-12">
 			<div className="product__pagination">
-				<a className="active" href="#">
+				<a className="active mr-1" href="#">
 					1
 				</a>
-				<a href="#">2</a>
-				<a href="#">3</a>
-				<span>...</span>
-				<a href="#">21</a>
+				<a className=" mr-1" href="#">
+					2
+				</a>
+				<a className=" mr-1" href="#">
+					3
+				</a>
+				<span className=" mr-1">...</span>
+				<a className=" mr-1" href="#">
+					21
+				</a>
 			</div>
 		</div>
 	);
