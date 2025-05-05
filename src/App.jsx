61a919cb12b642dc6821e119fd557a96c4f7193c
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-05-05 02:15:52 +0100

    Created Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./pages/Hero";
 import ProductList from "./pages/ProductList";
@@ -9,6 +10,7 @@ function App() {
 			<Header />
 			<Hero />
 			<ProductList />
+			<Footer />
 		</div>
 	);
 }
